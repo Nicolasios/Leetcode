0f48ac99a2acd7b97844cee06a46334ba011e889
Nicolasios <1498913522@qq.com>
2020-11-22 11:13:59 +0800

    problem 387 solved

---
+++ 387.字符串中的第一个唯一字符.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=cpp
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+
+// @lc code=start
+
+//反向查找
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        int strlen = s.length();
+        int firstUniqChar(string s)
+        {
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (s.find(s[i]) == s.rfind(s[i]))
+                    return i;
+            }
+            return -1;
+        }
+    }
+};
+// @lc code=end
