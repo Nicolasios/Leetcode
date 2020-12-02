ad9bd0e34f30cc3985443a8763a08d1460a3c0eb
Nicolasios <1498913522@qq.com>
2020-12-02 16:47:40 +0800

    problem 344 solved

---
+++ 344.反转字符串.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int n = s.size();
+        for (int left = 0, right = n - 1; left < right; ++left, --right)
+        {
+            swap(s[left], s[right]);
+        }
+    }
+};
+
+// @lc code=end
+
+//["a","m","a","n","a","P"," ",":","l","a","n","a","c"," "," ","a",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
+//["a","m","a","n","a","P"," ",":","l","a","n","a","c"," ","a"," ",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
