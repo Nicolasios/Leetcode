ed18b880fcd2df1ff6c835279000826f49ef3d77
Nicolasios <1498913522@qq.com>
2020-12-14 10:13:05 +0800

    problem 108 solved

---
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//递归建立平衡二叉树，每次都选择中间节点作为根节点递归建树
+class Solution
+{
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return helper(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode *helper(vector<int> &nums, int left, int right)
+    {
+        if (left > right)
+        {
+            return nullptr;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = helper(nums, left, mid - 1);
+        root->right = helper(nums, mid + 1, right);
+        return root;
+    }
+};
+
+// @lc code=end
